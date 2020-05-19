206bd1eeb762a26638191e878b029c426075b966
Gauthier Torreilles <gauthiertorreilles@gmail.com>
2020-05-19 11:28:48 +0200

    Création de la Navbar

--- src/Controller/Admin/HomeController.php
+++ src/Controller/Admin/HomeController.php
@@ -4,8 +4,14 @@
 namespace App\Controller\Admin;
 
 
+use App\Entity\Home;
+use App\Form\HomeType;
 use App\Repository\HomeRepository;
+use App\Repository\TeamRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,22 +22,56 @@ class HomeController extends AbstractController
      */
     private $homeRepository;
 
-    public function __construct(HomeRepository $homeRepository)
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var TeamRepository
+     */
+    private $temRepository;
+
+    public function __construct(HomeRepository $homeRepository, TeamRepository $teamRepository,  EntityManagerInterface $em)
     {
         $this->homeRepository = $homeRepository;
+        $this->temRepository = $teamRepository;
+        $this->em = $em;
     }
 
     /**
-     * @param HomeRepository $repository
      * @return Response
-     * @Route("/admin")
+     * @Route("/admin", name="admin.home")
      */
     public function index(){
-
-        $home = $this->homeRepository->find(1);
-
+        $description = $this->homeRepository->find(1);
+        $team = $this->temRepository->findAll();
         return $this->render('Admin/home/index.html.twig', [
-            'home' => $home
+            'description' => $description,
+            'team' => $team
+        ]);
+    }
+
+    /**
+     * @Route("/admin/description/edit/{id}", name="app.description.edit", methods="POST|GET")
+     * @param Request $request
+     * @param Home $home
+     * @return RedirectResponse|Response
+     */
+    public function edit(Request $request, Home $home){
+
+        $form = $this->createForm(HomeType::class, $home);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $this->em->flush();
+            $this->addFlash('success', 'Description modifiée avec succès');
+            return $this->redirectToRoute('admin.home');
+        }
+
+        return $this->render('Admin/home/edit.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
